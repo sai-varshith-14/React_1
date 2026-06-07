e635b440c5db2908f5d7848427a1f2215b842456
Varshith <varshitrajapu@gmail.com>
2026-06-07 14:13:24 +0530

    first push

---
+++ README.md
@@ -0,0 +1,2 @@
+This was the repository for my first react project
+This was my first introduction to React and this is just learning project.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+const App = () =>{
+  return (<h2>Functional Arrow Component</h2>);
+}
+
+export default App
