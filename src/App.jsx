d158eb7f7e4106a24e1b398e15bcdd575c7eeb3d
Varshith <varshitrajapu@gmail.com>
2026-06-09 11:30:14 +0530

    count updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-const Card = ( {name} ) => {
-  return (<div>
-    <h2>{name}</h2>
-  </div>)
-}
 
 const App = () =>{
+  const [count, setCount] = useState(0);
+    useEffect(() => {
+      document.title = `You have clicked ${count} times`
+    }, [count])
   return (
-    <div className = "Cardc">
-     <Card name="Avengers" />
-     <Card name="Para" />
-     <Card name="Hi" />
-    </div>
-    
+    <>
+    <div >The count is {count}</div>
 
-   
-  
+    <button onClick={() => {setCount(count+1)}}>Update count</button>
+    </>
 );
 }
 
